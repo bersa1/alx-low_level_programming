9d9ac95f27662e31ad900a5f0e4a77fc184fc0a7
bersabeh <bersagirma74@gmil.com>
2022-03-16 19:31:17 +0300

    prints number from 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the numbers starting from 0 - 9, followed by a new line
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+int x = 0;
+for (int x=0; x<10; x++){
+	printf("\x");
+}
+
+return (0);
+}
